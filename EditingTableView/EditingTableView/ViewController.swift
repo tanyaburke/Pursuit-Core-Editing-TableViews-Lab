0a9052251469d7ec70f88203f24e09b262d94219
tanyaburke <tanyaburke@pursuit.org>
2019-11-22 06:45:58 -0500

    Added Xcode File

---
+++ EditingTableView/EditingTableView/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  EditingTableView
+//
+//  Created by Tanya Burke on 11/21/19.
+//  Copyright © 2019 Tanya Burke. All rights reserved.
+//
+//# Pursuit-Core-Editing-TableViews-Lab
+//
+//## Grocery List
+//
+//For this app, you should build a grocery list and practice editing TableViews.
+//
+//## Requirements
+//
+//1. The user can add a new grocery list item
+//1. The user can delete items from their grocery list
+//1. Have two sections: one for unpurchased items, and one for purchased items
+//1. Tapping on a cell moves the item from one section to the other
+//1. Bonus: Add in handling to reorder the items within each section
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
